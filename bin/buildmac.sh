eff13523e40d9f396046a76ad078d5148c832e55
monsterkodi <monsterkodi@gmx.net>
2018-05-22 18:05:56 +0200

    mac fixes

--- bin/buildmac.sh
+++ bin/buildmac.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 cd `dirname $0`/..
 
-NAME=`sds productName`
+NAME=kappo
 
 2>/dev/null 1>/dev/null killall $NAME
 2>/dev/null 1>/dev/null killall $NAME
 
-konrad --run
+konrad 
 
 IGNORE="/(.*\.dmg$|Icon$|watch$|coffee$|bin/.*\.sh$|bin/.*\.noon$|bin/.*\.json$|icons$|.*md$|pug$|styl$|.*\.noon$|.*\.lock$|img/banner\.png|img/dmg|img/shot|img/.*\.pxm)"
 
 node_modules/electron-packager/cli.js . --overwrite --icon=img/$NAME.icns --ignore=$IGNORE
 
-rm $NAME-darwin-x64/LICENSE*
-rm $NAME-darwin-x64/version
+rm -f $NAME-darwin-x64/LICENSE*
+rm -f $NAME-darwin-x64/version
