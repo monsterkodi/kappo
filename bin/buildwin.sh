a5655b8c74bf75d36a87b8798aae5741cd8bf5ef
monsterkodi <monsterkodi@gmx.net>
2018-06-24 18:14:24 +0200

    build

--- bin/buildwin.sh
+++ bin/buildwin.sh
@@ -9,5 +9,7 @@ if rm -rf kappo-win32-x64; then
     
     IGNORE="/(.*\.dmg$|Icon$|watch$|coffee$|bin/.*\.sh$|bin/.*\.noon$|bin/.*\.json$|icons$|.*md$|pug$|styl$|.*\.noon$|.*\.lock$|img/banner\.png|img/dmg|img/shot|img/.*\.pxm)"
     
-    node_modules/electron-packager/cli.js . --overwrite --icon=img/kappo.ico --no-prune --ignore=$IGNORE --win32metadata.FileDescription=kappo
+    node_modules/electron-packager/cli.js . --overwrite --icon=img/app.ico --ignore=$IGNORE
+    
+    rm -rf kappo-win32-x64/resources/app/inno
 fi
